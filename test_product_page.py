040382453746d0d07cdf92858c76c419e8c8caa1
ivanuta_ag <spartak-champion@bk.ru>
2020-02-26 10:57:48 +0300

    review for final task.v2

--- test_product_page.py
+++ test_product_page.py
@@ -20,9 +20,6 @@ import time
 #          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    # print(link)
     page = ProductPage(browser, link)
     page.open()
     page.should_be_basket()
